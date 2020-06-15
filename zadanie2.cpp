153a8cce307b05d5546b5b57879b79744a8eff8a
mikolaj2002 <41815719+mikolaj2002@users.noreply.github.com>
2020-06-15 13:29:34 +0200

    Add files via upload

---
+++ zadanie2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void wypisz(string s)
+{
+	int i = 0;
+	while (s[i] == 1)
+		i++;
+	if (s[i] != 0)
+	{
+		cout << s[i];
+		s[i] = (char)1;
+		wypisz(s);
+	}
+}
+
+int main()
+{
+	string napis;
+	cout << "Podaj napis: ";
+	getline(cin, napis);
+
+	wypisz(napis);
+
+	return 0;
+}
